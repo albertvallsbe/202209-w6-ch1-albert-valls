dcee2ce7504a8e669efe9f9f6dc8ec40fa2d64f3
Albert Valls <avalls89@gmail.com>
2022-11-01 17:50:27 +0100

    Update fixes

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -1,8 +1,7 @@
-import { useAppSelector } from "../../app/hooks";
 import ToDoListStyled from "./ToDoListStyled";
 
 const ToDoList = () => {
-  const list = useAppSelector((state) => state.toDo.list);
+  // const list = useAppSelector((state) => state.toDo.list);
 
   return (
     <>
@@ -10,11 +9,6 @@ const ToDoList = () => {
         <header>
           <h1>To Do List Tasks</h1>
         </header>
-        <main>
-          {list.map(({ id, name, done }, index) => (
-            <ToDoCard key={index} id={id} name={name} done={done} />
-          ))}
-        </main>
       </ToDoListStyled>
     </>
   );
